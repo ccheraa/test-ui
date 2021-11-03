ff722ca64ade7f284600da2b0865d6d6f4208586
Charafeddine Cheraa <ccheraa@gmail.com>
2021-11-03 20:01:40 +0300

    added drag and drop test

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 $(function() {
-  console.log($);
-  console.log($('#login-form'));
   $('#login-form').submit(function(event) {
     event.preventDefault();
-    const username = $('#username-input').val();
-    const password = $('#password-input').val();
+    var username = $('#username-input').val();
+    var password = $('#password-input').val();
     $('.message', this).hide();
     if (username === 'root' && password === 'admin') {
-      const successMessage = $('.success', this);
+      var successMessage = $('.success', this);
       successMessage.show();
       setTimeout(function() {
         successMessage.fadeOut(1000);
@@ -16,6 +14,31 @@ $(function() {
       $('.error', this).show();
       $('#username-input').val('').focus();
       $('#password-input').val('');
+    }
+  });
+
+  $('#dnd .item').on('dragstart', function(event) {
+    event.originalEvent.dataTransfer.setData('fruit', $(this).attr('id'));
+    event.originalEvent.dataTransfer.setData('list', $(this).parent('.list').attr('id'));
+  });
+  $('#dnd .item').on('dragend', function(event) {
+    $('#dnd .list').removeClass('success bg-success');
+  });
+  $('#dnd .list').on('dragover', function(event) {
+    event.preventDefault();
+  });
+  $('#dnd .list, #dnd .list *').on('drop', function(event) {
+    var source = event.originalEvent.dataTransfer.getData('list');
+    var targetEl = $(event.target);
+    var target;
+    if (targetEl.hasClass('list')) {
+      target = targetEl.attr('id');
+    } else {
+      target = targetEl.parent('.list').attr('id');
+    }
+    if (source !== target) {
+      var fruit = event.originalEvent.dataTransfer.getData('fruit');
+      $('#' + target).append($('#' + fruit));
     }
   });
 });
